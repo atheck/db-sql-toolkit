35f360bd4c9877312d68ebfbe006350397fda810
Henrik Fuchs <atheck@gmail.com>
2023-03-23 20:21:02 +0100

    fix: export migrate function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { bulkExecuteCommand, BulkExecuteStatementParams, bulkGetRows, bulkInsertEntities, BulkStatementParams } from "./bulk";
 import { Database } from "./Database";
+import { migrate, MigrationOptions } from "./migrate";
 import { sql, sqlLiteral, StatementParams } from "./sql";
 
-export type { Database, BulkExecuteStatementParams, BulkStatementParams, StatementParams };
+export type { Database, BulkExecuteStatementParams, BulkStatementParams, MigrationOptions, StatementParams };
 
-export { bulkExecuteCommand, bulkGetRows, bulkInsertEntities, sql, sqlLiteral };
+export { bulkExecuteCommand, bulkGetRows, bulkInsertEntities, migrate, sql, sqlLiteral };
