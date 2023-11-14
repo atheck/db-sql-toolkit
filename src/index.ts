7cea7b77ba03ff5a7618246217700c9c40bbd455
Henrik Fuchs <atheck@gmail.com>
2023-11-14 18:32:11 +0100

    chore: update deps, use biome

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { bulkExecuteCommand, BulkExecuteStatementParams, bulkGetRows, bulkInsertEntities, BulkStatementParams } from "./bulk";
 import { Database } from "./Database";
-import { migrate, MigrationOptions } from "./migrate";
-import { sql, sqlLiteral, StatementParams } from "./sql";
+import { BulkExecuteStatementParams, BulkStatementParams, bulkExecuteCommand, bulkGetRows, bulkInsertEntities } from "./bulk";
+import { MigrationOptions, migrate } from "./migrate";
+import { StatementParams, sql, sqlLiteral } from "./sql";
 
 export type { Database, BulkExecuteStatementParams, BulkStatementParams, MigrationOptions, StatementParams };
 
